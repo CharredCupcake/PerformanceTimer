601541cb3f5610359d1e90d73229bc3778aa0ad6
MGSharp <m.n.groshev@gmail.com>
2020-11-26 14:53:27 +0200

    Separate stop and output function added.

--- PerformanceTimer.hpp
+++ PerformanceTimer.hpp
@@ -14,7 +14,7 @@ public:
 	{
 		m_start = std::chrono::high_resolution_clock::now();
 	};
-	~ChronoTimer() { stop(); };
+	~ChronoTimer() { stop(); output(); };
 
 	void stop()
 	{
@@ -23,14 +23,22 @@ public:
 			m_end = std::chrono::high_resolution_clock::now();
 			m_elapsed = std::chrono::time_point<std::chrono::nanoseconds>(m_end - m_start);
 		}
-
-		m_wasStopped = true;
-
-		*m_out << "Name:\n";
-		*m_out << m_name << '\n';
-		*m_out << "Time Elapsed:\n";
-		*m_out << m_elapsed.time_since_epoch().count() / 1000000 << " ms\n";
+		else
+		{
+			m_wasStopped = true;
+		}
 	};
+
+	void output()
+	{
+		if (m_wasStopped)
+		{
+			*m_out << "Name:\n";
+			*m_out << m_name << '\n';
+			*m_out << "Time Elapsed:\n";
+			*m_out << m_elapsed.time_since_epoch().count() / 1000000 << " ms\n";
+		}
+	}
 
 private:
 
